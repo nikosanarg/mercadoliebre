b29b136919a301632dca068428aca5591ab6d070
Nicolas Sandobal <nicsandobal@gmail.com>
2021-11-04 17:12:03 -0300

    Agregar componentes con ESJ

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
+const productos = require("./functions.js");
 
 const app = express();
-
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
+  //res.sendFile(path.join(__dirname, "./views/home.html"));
+  res.render("home", { productos });
 });
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/register.html"));
+app.get("/contacto", (req, res) => {
+  //res.sendFile(path.join(__dirname, "./views/contacto.html"));
+  res.render("contacto");
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/login.html"));
+app.get("/galeria", (req, res) => {
+  //res.sendFile(path.join(__dirname, "./views/galeria.html"));
+  res.render("galeria");
+});
+
+app.get("/carrito", (req, res) => {
+  //res.sendFile(path.join(__dirname, "./views/carrito.html"));
+  res.render("carrito");
 });
 
 app.listen(3030, () => {
